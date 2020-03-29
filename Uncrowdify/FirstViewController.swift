2551fc8f7b754606c2eb2daa9372f170b75d1c40
ikaliko <ikaliko@gmail.com>
2020-03-28 22:19:07 -0700

    Initial Commit

---
+++ Uncrowdify/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Uncrowdify
+//
+//  Created by Ilia Kaliko on 3/28/20.
+//  Copyright © 2020 Obsessive Coders, Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Uncrowdify/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Uncrowdify
+//
+//  Created by Ilia Kaliko on 3/28/20.
+//  Copyright © 2020 Obsessive Coders, Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
